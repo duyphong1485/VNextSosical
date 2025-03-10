2904255f5f336afadfbadc1402a7dd0e184dcfee
Quang DO <quangteo06061999@gmail.com>
2025-03-10 12:49:41 +0900

    Post User

--- VnextSNS_API/VnextSNS_API/vnext_app/urls.py
+++ VnextSNS_API/VnextSNS_API/vnext_app/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LoginView, RegisterView
+from .views import LoginView, RegisterView , get_post_detail , create_post, get_post,delete_post
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    # -------------path quang------------------------
+    path('posts/',get_post, name = 'list-post'),
+    path('posts/create/',create_post, name = 'create-post'),
+    path('posts/detail/<int:postID>',get_post_detail, name = 'detail-post'),
+    path('posts/delete/<int:postID>',delete_post, name = 'detail-post'),
+    path('api-token/', obtain_auth_token, name='api_token'),
+    # -------------end path quang------------------------
+
 ]
