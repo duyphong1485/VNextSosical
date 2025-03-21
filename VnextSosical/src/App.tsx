beb12bec29b4ba264f69cc12afbcf12fbf12a864
Trongvn1503 <ductrong15032001@gmail.com>
2025-03-21 16:50:17 +0900

    testcardlist

--- VnextSosical/src/App.tsx
+++ VnextSosical/src/App.tsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
 import SignUpPage from './pages/SignUpPage'
 import LoginPage from './pages/SignInPage'
+import CardList from "./components/Card/CardList";
+import Card from "./components/Card/Card";
+import Card2 from "./components/Card/Card2";
 function App() {
 
   return (
@@ -8,6 +11,17 @@ function App() {
 			<Routes>
         <Route path='/sign-in' element={<LoginPage />} />
 				<Route path="/sign-up" element={<SignUpPage />}/>
+        <Route
+        path="/cardlist"
+        element={
+          <CardList>
+            <Card secondary />
+            <Card2 fontSize="20px" secondary />
+            <Card />
+            <Card2 />
+          </CardList>
+        }
+      />
 			</Routes>
     </>
   )
---
+++ VnextSosical/src/components/Card/Card.tsx
@@ -0,0 +1,133 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+interface CardProps {
+  secondary?: boolean;
+}
+
+const StyledCard = styled.div`
+  position: relative;
+`;
+
+const CardImage = styled.div`
+  height: 400px;
+  width: 100%;
+  border-radius: 8px;
+`;
+
+const CardImg = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: inherit;
+`;
+
+const CardContent = styled.div`
+  position: absolute;
+  width: calc(100% - 36px);
+  left: 50%;
+  bottom: 0;
+  transform: translate(-50%, 50%);
+  background-color: white;
+  z-index: 10;
+  border-radius: 20px;
+  padding: 20px;
+`;
+
+const CardTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const CardUser = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 12px;
+`;
+
+const UserAvatar = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 100rem;
+  object-fit: cover;
+  flex-shrink: 0;
+`;
+
+const UserName = styled.span`
+  font-weight: 300;
+  font-size: 16px;
+`;
+
+const CardFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const CardTitle = styled.h3`
+  font-size: 18px;
+  font-weight: 500;
+ 
+`;
+
+const CardAmount = styled.span<CardProps>`
+  font-size: 18px;
+  font-weight: bold;
+  background: linear-gradient(
+    86.88deg,
+    #7d6aff 1.38%,
+    #ffb86c 64.35%,
+    #fc2872 119.91%
+  );
+  ${(props) =>
+    props.secondary &&
+    css`
+      background: linear-gradient(86.88deg, #20e3b2, #2cccff);
+    `};
+  color: transparent;
+  -webkit-background-clip: text;
+  background-clip: text;
+`;
+
+const CardMeta = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 12px;
+`;
+
+const Card: React.FC<CardProps> = ({ secondary }) => {
+  return (
+    <StyledCard>
+      <CardImage>
+        <CardImg
+          src="https://cdn.dribbble.com/users/2400293/screenshots/19060197/media/82d672bd58929b313f4805df5e48d586.png?compress=1&resize=400x300&vertical=top"
+          alt=""
+        />
+      </CardImage>
+      <CardContent>
+        <CardTop>
+          <CardUser>
+            <UserAvatar
+              src="anhdaidien.png"
+              alt=""
+            />
+            <UserName>@zndrson</UserName>
+          </CardUser>
+          <CardMeta>
+            <img src="/icon-heart.svg" alt="heart" />
+            <span>256</span>
+          </CardMeta>
+        </CardTop>
+        <CardFooter>
+          <CardTitle>Cosmic Perspective</CardTitle>
+          <CardAmount secondary={secondary}>12,000 PSL</CardAmount>
+        </CardFooter>
+      </CardContent>
+    </StyledCard>
+  );
+};
+
+export default Card;
